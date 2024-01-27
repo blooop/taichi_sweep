bb578247ec62a8401ae3e46f33254f31637ce2ab
Austin Gregg-Smith <blooop@gmail.com>
2024-01-27 18:58:25 +0000

    remove unused dependencies

--- scripts/launch_vscode.sh
+++ scripts/launch_vscode.sh
@@ -2,18 +2,15 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" #gets the current directory
-cd $SCRIPT_DIR/..
+cd "$SCRIPT_DIR"/..
 
 CONTAINER_NAME=${PWD##*/}  
 
-echo "stopping existing container" $CONTAINER_NAME 
-docker stop $CONTAINER_NAME || true
+echo "stopping existing container" "$CONTAINER_NAME" 
+docker stop "$CONTAINER_NAME" || true 
 
-container_hex_id=$(printf $CONTAINER_NAME | xxd -p | tr '\n' ' ' | sed 's/\\s//g' | tr -d ' ');
+CONTAINER_HEX=$(printf $CONTAINER_NAME | xxd -p | tr '\n' ' ' | sed 's/\\s//g' | tr -d ' ');
 
-# rocker --x11 --user --home --pull --name $CONTAINER_NAME --mode non-interactive ubuntu:22.04 
-# rocker --x11 --user --home --pull --name $CONTAINER_NAME ubuntu:22.04 
-rocker --nvidia --x11 --user --home --pull --git --name $CONTAINER_NAME --oyr-run-arg " --detach" --deps-dependencies ubuntu:22.04 
+rocker --nvidia --x11 --user --pull --git --name "$CONTAINER_NAME" --volume "${PWD}":/workspaces/"${CONTAINER_NAME}":Z --oyr-run-arg " --detach" --deps-dependencies ubuntu:22.04 
 
-
-code --folder-uri vscode-remote://attached-container+$container_hex_id/${PWD}
+code --folder-uri vscode-remote://attached-container+"$CONTAINER_HEX"/workspaces/"${CONTAINER_NAME}"
